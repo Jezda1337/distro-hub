77813afc362c1e9f7f60b082a400fa04b23f35ca
Radoje <megasrbin1@gmail.com>
2023-02-15 23:03:19 +0100

    UI changes for individual disto

--- src/pages/[distro]/index.tsx
+++ src/pages/[distro]/index.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/Button";
 import { dehydrate, QueryClient, useQuery } from "@tanstack/react-query";
+// @ts-ignore
+import { CldImage } from "next-cloudinary";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 async function getDistro({ queryKey }: any) {
@@ -32,12 +36,38 @@ export default function Distro() {
     queryFn: getDistro,
   });
 
-  console.log(data);
+  const colors = ["red", "green", "blue"];
 
   return (
-    <>
-      <h1>hello world</h1>
-      {data.name}
-    </>
+    <section className="my-20">
+      <div className="my-20 h-[300px] w-full bg-red-100"></div>
+      <div className="flex items-end">
+        <CldImage
+          alt="test"
+          src={data.logo}
+          width="0"
+          height="0"
+          className="h-12 w-12"
+          format="svg"
+        />
+        <h2 className=" ml-4 text-2xl">{data.name}</h2>
+
+        <div className="ml-auto flex gap-4">
+          <Button className="">
+            <Link href={data.website}>web</Link>
+          </Button>
+          <Button className="">
+            {/* href need change */}
+            <Link href={data.download || ""}>download</Link>
+          </Button>
+        </div>
+      </div>
+      <div className="mt-20">
+        <h2 className=" relative my-4 text-2xl first-letter:uppercase after:absolute after:top-1/2 after:ml-5 after:h-[3px] after:w-12 after:-translate-y-1/2 after:rounded-full after:bg-black">
+          About
+        </h2>
+        <p className="">{data.description}</p>
+      </div>
+    </section>
   );
 }
