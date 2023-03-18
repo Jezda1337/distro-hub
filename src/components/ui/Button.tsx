d312fdbc29cd168a622ac23dd361bc8f1fc4de70
Radoje <megasrbin1@gmail.com>
2023-03-18 14:46:34 +0100

    fix animation on button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { twMerge } from "tailwind-merge"
 
-const defaultStyle = `relative isolate overflow-hidden rounded border border-black bg-white px-3 py-1 transition-all after:absolute after:inset-0 after:-z-10 after:block after:origin-bottom after:scale-y-0 after:rounded-t-[10rem] after:bg-black after:transition-[border-radius, transform] after:duration-[0.4s] hover:text-white hover:after:scale-y-100 hover:after:rounded-none hover:transition-all hover:duration-[400ms] hover:bg-black active:scale-100 active:transition-transform`
+const defaultStyle = `"after:transition-[border-radius, transform] relative isolate overflow-hidden rounded border border-black bg-white px-3 py-1 transition-all after:absolute after:inset-0 after:-z-10 after:block after:origin-bottom after:scale-y-0 after:rounded-t-[10rem] after:bg-black after:duration-[0.4s] hover:scale-110 hover:bg-black hover:text-white hover:transition-all hover:duration-[400ms] hover:after:scale-y-100 hover:after:rounded-none active:scale-100 active:transition-transform active:delay-75"`
 
 const Button = React.forwardRef<
 	HTMLButtonElement,
