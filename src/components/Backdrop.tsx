64b30a0badfe0001b48cf09ee11ef85d4638510c
Radoje <megasrbin1@gmail.com>
2023-03-30 12:01:37 +0200

    add - tiny animations for menu button, and escape fro m modal using ESC key

--- src/components/Backdrop.tsx
+++ src/components/Backdrop.tsx
@@ -1,14 +1,23 @@
 import { motion } from "framer-motion"
+import { useEffect } from "react"
 
 export default function Backdrop({ children, handleClose }: any) {
-	return (
-		<motion.div
-			onClick={handleClose}
-			className="absolute inset-0 z-10 grid place-items-center bg-black/50 backdrop-blur-sm"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}>
-			{children}
-		</motion.div>
-	)
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Escape") {
+        handleClose();
+      }
+    })
+    return window.removeEventListener("keydown", handleClose)
+  }, [])
+  return (
+    <motion.div
+      onClick={handleClose}
+      className="absolute inset-0 z-10 grid place-items-center bg-black/50 backdrop-blur-sm"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}>
+      {children}
+    </motion.div>
+  )
 }
--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -5,72 +5,72 @@ import { useEffect, useRef, useState } from "react"
 import { Button } from "./ui/Button"
 
 export default function DropdownMenu() {
-	const [open, setOpen] = useState(false)
-	const menuRef = useRef<HTMLDivElement | null>(null)
+  const [open, setOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement | null>(null)
 
-	useEffect(() => {
-		document.addEventListener(
-			"click",
-			(e: MouseEvent) => {
-				if (!menuRef.current?.contains(e.target as HTMLElement)) {
-					setOpen(false)
-				}
-			},
-			true
-		)
-	}, [])
+  useEffect(() => {
+    document.addEventListener(
+      "click",
+      (e: MouseEvent) => {
+        if (!menuRef.current?.contains(e.target as HTMLElement)) {
+          setOpen(false)
+        }
+      },
+      true
+    )
+  }, [])
 
-	return (
-		<div
-			tabIndex={0}
-			ref={menuRef}
-			className="relative">
-			<Button onClick={() => setOpen(!open)}>
-				{!open ? (
-					<Bars3BottomRightIcon className="h-6 w-6" />
-				) : (
-					<XMarkIcon className="h-6 w-6" />
-				)}
-			</Button>
-			<AnimatePresence>
-				{open ? (
-					<motion.div
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						tabIndex={0}
-						className="absolute right-0 z-10 mt-3 min-w-[200px] rounded border bg-white shadow">
-						<ul
-							className="py-2"
-							tabIndex={0}>
-							<li className="border-b px-5 pb-2">
-								<p className="font-medium">Menu</p>
-							</li>
-							<li onClick={() => setOpen(false)}>
-								<Link
-									className="block px-5 py-2 hover:bg-black hover:text-white"
-									href={"/waiting-list"}>
-									Waiting list
-								</Link>
-							</li>
-							<li onClick={() => setOpen(false)}>
-								<Link
-									className="block px-5 py-2 hover:bg-black hover:text-white"
-									href={"/about"}>
-									About
-								</Link>
-							</li>
-							<li onClick={() => setOpen(false)}>
-								<Link
-									className="block px-5 py-2 hover:bg-black hover:text-white"
-									href={"/auth/login"}>
-									Admin login
-								</Link>
-							</li>
-						</ul>
-					</motion.div>
-				) : null}
-			</AnimatePresence>
-		</div>
-	)
+  return (
+    <div
+      tabIndex={0}
+      ref={menuRef}
+      className="relative">
+      <Button onClick={() => setOpen(!open)}>
+        {!open ? (
+          <Bars3BottomRightIcon className="h-6 w-6" />
+        ) : (
+          <XMarkIcon className="h-6 w-6" />
+        )}
+      </Button>
+      <AnimatePresence>
+        {open ? (
+          <motion.div
+            initial={{ scale: 0, originX: "100%", originY: 0 }}
+            animate={{ scale: 1 }}
+            exit={{ scale: 0 }}
+            tabIndex={0}
+            className="absolute right-0 z-10 mt-3 min-w-[200px] rounded border bg-white shadow">
+            <ul
+              className="py-2"
+              tabIndex={0}>
+              <li className="border-b px-5 pb-2">
+                <p className="font-medium">Menu</p>
+              </li>
+              <li onClick={() => setOpen(false)}>
+                <Link
+                  className="block px-5 py-2 hover:bg-black hover:text-white"
+                  href={"/waiting-list"}>
+                  Waiting list
+                </Link>
+              </li>
+              <li onClick={() => setOpen(false)}>
+                <Link
+                  className="block px-5 py-2 hover:bg-black hover:text-white"
+                  href={"/about"}>
+                  About
+                </Link>
+              </li>
+              <li onClick={() => setOpen(false)}>
+                <Link
+                  className="block px-5 py-2 hover:bg-black hover:text-white"
+                  href={"/auth/login"}>
+                  Admin login
+                </Link>
+              </li>
+            </ul>
+          </motion.div>
+        ) : null}
+      </AnimatePresence>
+    </div>
+  )
 }
